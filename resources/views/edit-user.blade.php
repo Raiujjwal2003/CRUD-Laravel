b364e488abc157555d8e251b882b91706c48aeb8
Raiujjwal2003 <raiujjwal2003@gmail.com>
2024-04-12 21:29:41 +0530

    fix bug

--- resources/views/edit-user.blade.php
+++ resources/views/edit-user.blade.php
@@ -17,7 +17,7 @@
 </head>
 <body>
 
-<!-- @extends('layout.navbar') -->
+
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
             <a class="navbar-brand" href="#">My Form</a>
